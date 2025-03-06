d75d69d8c76025b0070031d4b2db301886af746c
priyanka9913 <rathipriyanka1399@gmail.com>
2025-03-06 22:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function stringChop(str, size) {
   // your code here
+	let arr = [];
+  let res = "";
+  if(str == null || str == ""){
+    return [];
+  }
+  if(str.length < size){
+    arr.push(str);
+    return arr;
+  }
+  for(let i = 0; i < str.length; i++){
+    res += str.charAt(i);
+    if(res.length == size){
+      arr.push(res);
+      res = "";
+    }
+  }
+  if(res.length != 0){
+    arr.push(res);
+  }
+  return arr;
+	
 }
 
 // Do not change the code below
